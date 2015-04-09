208b3f9a2ad21f138e1b2c8ff18bef12f218be37
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2015-04-09 13:34:37 +0300

    Initial commit

---
+++ 4.3.5/download-shopware.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_4.3.5_06cc302f7f0db92e8e40d8a9fc4190ec84c3314b.zip
---
+++ 4.3.5/sql-data.php
@@ -0,0 +1,9 @@
+<?php
+
+$sql_files = array(
+    '/swrecovery/install/data/sql/sw4_clean.sql',
+    '/swrecovery/install/data/sql/snippets.sql',
+    '/swrecovery/install/data/sql/en.sql',
+    '/swtools/create-admin.sql',
+);
+
