e578261d3b4fe17d063b2dc74bfa593efcb76add
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2015-04-09 18:51:10 +0300

    Added Shopware shops 4.1.*

---
+++ 4.1.1/sql-data.php
@@ -0,0 +1,8 @@
+<?php
+
+$sql_files = array(
+    '/swinstall/assets/sql/sw4_clean.sql',
+    '/swinstall/assets/sql/en.sql',
+    '/swtools/create-admin.sql',
+);
+
---
+++ 4.1.4/download-shopware.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O /shopware/shop.zip http://releases.s3.shopware.com/install_4.1.4.zip
