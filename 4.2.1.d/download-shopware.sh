c61141d7e87361f53ffbd11f2b7303a5ed9f141e
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2015-04-10 10:32:23 +0300

    Added Shopware shops with demo data

---
+++ 4.2.1.d/download-shopware.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O /shopware/shop.zip http://releases.s3.shopware.com/install_4.2.1.zip
+
+wget -O /shopware/demo.zip http://releases.s3.shopware.com/demo_4.2.0.zip
---
+++ 4.3.0.d/download-shopware.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O /shopware/shop.zip http://releases.s3.shopware.com/install_4.3.0_3194284222dda5693ef5ec2c65b5dce378213fb0.zip
+
+wget -O /shopware/demo.zip http://releases.s3.shopware.com/demo_4.3.0.zip
