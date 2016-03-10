aa9b9ebbd0b4d3bce17e0aaf1a9adeb932ebb1d5
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-03-10 18:52:59 +0200

    Added version 5.0.0

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_4.3.6_351e326e20363bfe1ba1f3bd821b870dd078d8f4.zip
-
-wget -O /shopware/demo.zip http://releases.s3.shopware.com/demo_4.3.0.zip
+wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.0.0_c122de4b3eba2d45f1085cc8df74ff96804179ec.zip
--- sql-data.php
+++ sql-data.php
@@ -1,7 +1,7 @@
 <?php
 
 $sql_files = array(
-    '/swrecovery/install/data/sql/sw4_clean.sql',
+    '/swrecovery/install/data/sql/install.sql',
     '/swrecovery/install/data/sql/snippets.sql',
     '/swrecovery/install/data/sql/en.sql',
     '/swtools/create-admin.sql',
