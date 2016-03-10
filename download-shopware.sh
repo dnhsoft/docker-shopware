f5db02bd04b2f169524d83530d681adde03c3d09
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-03-10 18:52:59 +0200

    Added version 4.3.6.d

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_4.3.6_351e326e20363bfe1ba1f3bd821b870dd078d8f4.zip
+
+wget -O /shopware/demo.zip http://releases.s3.shopware.com/demo_4.3.0.zip
