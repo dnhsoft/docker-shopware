608900cfa03e50408553253a55d0f3020b524e16
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-04-12 10:19:40 +0300

    Added version 5.1.5; examples updated

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.1.4_2a6c1d025af042525558ba10d738eeb19ed75ff4.zip
+
+wget -O /shopware/shop.zip  http://releases.s3.shopware.com.s3.amazonaws.com/install_5.1.5_ca9dc97e4751d0cd22d08548c7efe4edc6124f39.zip
--- examples/production-shop/init.sh
+++ examples/production-shop/init.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 docker exec productionshop_shop_1 /swtools/init.sh
-docker exec productionshop_shop_1 bash -c "chown -R www-data:www-data /shopware/engine/Shopware/Plugins/Community"
-docker exec productionshop_shop_1 bash -c "chmod -R 755 /shopware/engine/Shopware/Plugins/Community"
+
