465e53b73e502ba116b828af8bff633de54e15ff
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2016-06-30 11:44:37 +0300

    5.2RC3 added

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 
-wget -O /shopware/shop.zip  http://releases.s3.shopware.com.s3.amazonaws.com/install_5.1.6_04ec396ac8d2fa8c1e088bc2bd2c8132ab56c270.zip
+wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.0_RC3_c41fb094ff26a372ecf89d4523953c6ef1774de3.zip
