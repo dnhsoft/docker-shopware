110980ab54275d5033bb1fe42e1a12f5d2de78a8
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2017-02-15 10:59:44 +0200

    Version 5.2.18

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O /shopware/shop.zip  http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.17_582842251fc465c404e74b622e2638233d353d71.zip
+wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.2.18_40832027120b85c9620e6651b4a9b32290e10a22.zip
