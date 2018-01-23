a62bd613d40ad6c0092a32009bf38f643f34f2a0
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2018-01-23 12:23:11 +0200

    add version 5.3.7

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.6_c3d6cecc4dcbd0480a289006fdd4c643bfddb49c.zip
+wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.7_741ae9fb77ecb227dc6be9c1028ded1e957c0e14.zip
