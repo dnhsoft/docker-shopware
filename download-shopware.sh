3c32b71f8022b84a97c5e805d10637cebd53af11
Nikola Ivanovski <nikola.ivanovski@dnhsoft.com>
2018-02-06 13:37:20 +0200

    Version 5.4.0.RC1-php7

--- download-shopware.sh
+++ download-shopware.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.3.7_741ae9fb77ecb227dc6be9c1028ded1e957c0e14.zip
+wget -O /shopware/shop.zip http://releases.s3.shopware.com.s3.amazonaws.com/install_5.4.0_RC1_a6bfc3c1093beff91e0e9e49adf2dda8b7ef2de8.zip
