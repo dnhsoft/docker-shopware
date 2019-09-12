7e3eef2c29477438b2995aab04e0a1a72480499a
Chudomir Delchev <chudomir.delchev@dnhsoft.com>
2019-09-12 14:00:58 +0300

    Set latest to be 5.5.7; removed useless files;

--- swtools/download-shopware.sh
+++ swtools/download-shopware.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 if [ "$SHOPWARE_VERSION" = "latest"  ]; then
-  DOWNLOAD_URL="http://releases.shopware.com/install_5.5.10_edfcb8e82f331fa5a0935a6c6ff35fe4348bf262.zip"
-elif [ "$SHOPWARE_VERSION" = "5.5.10"  ]; then
-  DOWNLOAD_URL="http://releases.shopware.com/install_5.5.10_edfcb8e82f331fa5a0935a6c6ff35fe4348bf262.zip"
-elif [ "$SHOPWARE_VERSION" = "5.5.9"  ]; then
-  DOWNLOAD_URL="https://releases.shopware.com/install_5.5.9_402d088c6bc5a8c6f94cd785b046f081d664b3f8.zip"
-elif [ "$SHOPWARE_VERSION" = "5.5.8"  ]; then
-  DOWNLOAD_URL="https://releases.shopware.com/install_5.5.8_d5bf50630eeaacc6679683e0ab0dcba89498be6d.zip"
+  DOWNLOAD_URL="https://releases.shopware.com/install_5.5.7_f785facc70e39f2ca4292e78739457417f19fbcf.zip"
+#   DOWNLOAD_URL="http://releases.shopware.com/install_5.5.10_edfcb8e82f331fa5a0935a6c6ff35fe4348bf262.zip"
+# elif [ "$SHOPWARE_VERSION" = "5.5.10"  ]; then
+#   DOWNLOAD_URL="http://releases.shopware.com/install_5.5.10_edfcb8e82f331fa5a0935a6c6ff35fe4348bf262.zip"
+# elif [ "$SHOPWARE_VERSION" = "5.5.9"  ]; then
+#   DOWNLOAD_URL="https://releases.shopware.com/install_5.5.9_402d088c6bc5a8c6f94cd785b046f081d664b3f8.zip"
+# elif [ "$SHOPWARE_VERSION" = "5.5.8"  ]; then
+#   DOWNLOAD_URL="https://releases.shopware.com/install_5.5.8_d5bf50630eeaacc6679683e0ab0dcba89498be6d.zip"
 elif [ "$SHOPWARE_VERSION" = "5.5.7"  ]; then
   DOWNLOAD_URL="https://releases.shopware.com/install_5.5.7_f785facc70e39f2ca4292e78739457417f19fbcf.zip"
 else
